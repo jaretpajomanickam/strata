91be616e5a677e05156013670cd4316b29d7a5bb
jaretpajomanickam <jaretpajomanickam@gmail.com>
2025-04-05 15:04:27 +1100

    Fix: Move configuration files to root and clean up directory structure

--- next.config.js
+++ next.config.js
@@ -1,1 +1,1 @@
- 
+/** @type {import('next').NextConfig} */ const nextConfig = { images: { remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }] }, typescript: { ignoreBuildErrors: true }, eslint: { ignoreDuringBuilds: true } }; module.exports = nextConfig
